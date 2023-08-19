21d93b4ca561095cfdcbdbcb334482e4624a2903
Kalvin <kalvin.sgb@outlook.com>
2023-08-19 20:40:56 +0200

    🐛 : fix link '/DisneyplusClone/'

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/DisneyplusClone/" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
