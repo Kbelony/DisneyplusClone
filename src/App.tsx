3e806e93b6bb49b91f1f3153e44dd0988fcad734
Kalvin <kalvin.sgb@outlook.com>
2023-08-19 20:56:25 +0200

    🚀 : modification system route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../src/assets/style.scss";
 import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/DisneyplusClone/" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/DisneyplusClone/" element={<Login />} />
+      </Routes>
     </div>
   );
 }
