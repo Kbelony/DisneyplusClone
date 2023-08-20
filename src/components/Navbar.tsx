ce618de7b44986044d61522fb67b7a01f4d00181
Kalvin <kalvin.sgb@outlook.com>
2023-08-20 19:33:08 +0200

    🚧 : add img if connection google

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,11 @@
+import { auth, provider } from "../firebase";
+import { signInWithPopup } from "firebase/auth";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectUserName,
+  selectUserPhoto,
+  setUserLoginDetails,
+} from "../features/user/userSlice";
 import logo from "/assets/images/logo.png";
 import homeicon from "/assets/images/home-icon.svg";
 import searchicon from "/assets/images/search-icon.svg";
@@ -5,15 +13,31 @@ import watchlisticon from "/assets/images/watchlist-icon.svg";
 import originalicon from "/assets/images/original-icon.svg";
 import movieicon from "/assets/images/movie-icon.svg";
 import seriesicon from "/assets/images/series-icon.svg";
-import { auth, provider } from "../firebase";
-import { signInWithPopup } from "firebase/auth";
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  const userPhoto = useSelector(selectUserPhoto);
   const HandleAuth = () => {
-    signInWithPopup(auth, provider).then((result) => {
-      console.log(result);
-    });
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        setUser(result.user);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
+
+  const setUser = (user) => {
+    dispatch(
+      setUserLoginDetails({
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL,
+      })
+    );
+  };
+
   return (
     <nav>
       <div className="navbar-component">
@@ -25,63 +49,77 @@ const Navbar = () => {
               </picture>
             </a>
           </li>
-          <div className="menu hidden md:flex">
-            <li className="nav-list-item ">
-              <a href="/">
-                <div className="mr-4 p-2 flex">
-                  <img className="w-8" src={homeicon} alt="" />
-                  <span className="pt-1.5">HOME</span>
-                </div>
-              </a>
-            </li>
-            <li className="nav-list-item">
-              <a href="/">
-                <div className="mr-4 p-2 flex">
-                  <img className="w-8" src={searchicon} alt="" />
-                  <span className="pt-1.5">SEARCH</span>
-                </div>
-              </a>
-            </li>
-            <li className="nav-list-item">
-              <a href="/">
-                <div className="mr-4 p-2 flex">
-                  <img className="w-8" src={watchlisticon} alt="" />
-                  <span className="pt-1.5">WATCHLIST</span>
-                </div>
-              </a>
-            </li>
-            <li className="nav-list-item">
-              <a href="/">
-                <div className="mr-4 p-2 flex">
-                  <img className="w-8" src={originalicon} alt="" />
-                  <span className="pt-1.5">ORIGINALS</span>
-                </div>
-              </a>
-            </li>
-            <li className="nav-list-item">
-              <a href="/">
-                <div className="mr-4 p-2 flex">
-                  <img className="w-8" src={movieicon} alt="" />
-                  <span className="pt-1.5">MOVIES</span>
-                </div>
-              </a>
-            </li>
-            <li className="nav-list-item">
-              <a href="/">
-                <div className="mr-4 p-2 flex">
-                  <img className="w-8" src={seriesicon} alt="" />
-                  <span className="pt-1.5">SERIES</span>
-                </div>
-              </a>
-            </li>
-          </div>
-          <li className="nav-list-item ml-auto">
-            <a onClick={HandleAuth}>
-              <div className="login-button mr-9 p-2">
-                <span>s'identifier</span>
+          {userName ? (
+            <>
+              <div className="menu hidden md:flex">
+                <li className="nav-list-item ">
+                  <a href="/">
+                    <div className="mr-4 p-2 flex">
+                      <img className="w-8" src={homeicon} alt="" />
+                      <span className="pt-1.5">HOME</span>
+                    </div>
+                  </a>
+                </li>
+                <li className="nav-list-item">
+                  <a href="/">
+                    <div className="mr-4 p-2 flex">
+                      <img className="w-8" src={searchicon} alt="" />
+                      <span className="pt-1.5">SEARCH</span>
+                    </div>
+                  </a>
+                </li>
+                <li className="nav-list-item">
+                  <a href="/">
+                    <div className="mr-4 p-2 flex">
+                      <img className="w-8" src={watchlisticon} alt="" />
+                      <span className="pt-1.5">WATCHLIST</span>
+                    </div>
+                  </a>
+                </li>
+                <li className="nav-list-item">
+                  <a href="/">
+                    <div className="mr-4 p-2 flex">
+                      <img className="w-8" src={originalicon} alt="" />
+                      <span className="pt-1.5">ORIGINALS</span>
+                    </div>
+                  </a>
+                </li>
+                <li className="nav-list-item">
+                  <a href="/">
+                    <div className="mr-4 p-2 flex">
+                      <img className="w-8" src={movieicon} alt="" />
+                      <span className="pt-1.5">MOVIES</span>
+                    </div>
+                  </a>
+                </li>
+                <li className="nav-list-item">
+                  <a href="/">
+                    <div className="mr-4 p-2 flex">
+                      <img className="w-8" src={seriesicon} alt="" />
+                      <span className="pt-1.5">SERIES</span>
+                    </div>
+                  </a>
+                </li>
               </div>
-            </a>
-          </li>
+              <li className="nav-list-item ml-auto">
+                <a>
+                  <img
+                    className="user-photo rounded-full w-16 mr-6 mb-2"
+                    src={userPhoto}
+                    alt={userName}
+                  />
+                </a>
+              </li>
+            </>
+          ) : (
+            <li className="nav-list-item ml-auto">
+              <a onClick={HandleAuth}>
+                <div className="login-button mr-9 p-2">
+                  <span>s'identifier</span>
+                </div>
+              </a>
+            </li>
+          )}
         </ul>
       </div>
     </nav>
